8b8e36866ecda7d9d9ed37cdd3a3edaffc7f2b41
Adeniyii <ifedayoadeniyi@gmail.com>
2021-10-22 23:28:43 +0100

    :sparkles: [feat] setup logging using morgan and winston

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 require('dotenv').config();
 require('./config/database').connect();
+
+const cors = require('cors');
+const morgan = require('morgan');
+const express = require('express');
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('common'));
+
+app.use('/', (_, res) => res.send('yah yah'));
+
+module.exports = app;
