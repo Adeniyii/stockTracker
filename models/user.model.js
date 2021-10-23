26d1b07a138baccde1178cbf26cb97525b90a756
Adeniyii <ifedayoadeniyi@gmail.com>
2021-10-23 01:26:16 +0100

    :sparkles: [feat] build user schema and model

---
+++ models/user.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = new mongoose.Schema(
+  {
+    last_name: { type: String, default: null },
+    first_name: { type: String, default: null },
+    email_verified: { type: Boolean, default: false },
+    email: { type: String, unique: true, sparse: true },
+    phone_number_verified: { type: Boolean, default: false },
+    phone_number: { type: String, unique: true, sparse: true },
+    is_super_admin: { type: Boolean, default: false },
+    password: { type: String },
+    access_token: {
+      type: String,
+      unique: true,
+      sparse: true,
+    },
+    refresh_token: {
+      type: String,
+      unique: true,
+      sparse: true,
+    },
+  },
+  { timestamps: true },
+);
+
+// Format unique index mongo error to readable object
+userSchema.plugin(uniqueValidator);
+module.exports = mongoose.model('user', userSchema);
