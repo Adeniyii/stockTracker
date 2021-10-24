309aafed575b53c3c872406ec9d022c945b8c346
Adeniyii <ifedayoadeniyi@gmail.com>
2021-10-24 11:59:09 +0100

    :recycle: [refactor] use proper error handling
    I updated the auth controller to make use of the freshly implemented error handlers.. also fixed small bugs.

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,62 +1,58 @@
 const bcrypt = require('bcryptjs');
-// eslint-disable-next-line no-unused-vars
-const { request, response, NextFunction } = require('express');
+const AppError = require('../util/appError');
+const catchAsync = require('../util/catchAsync');
+const { successResponse } = require('../util/response_handler');
 const { loginService } = require('../services/AuthService/login.service');
 const { signUpService } = require('../services/AuthService/signup.service');
 const { userExists } = require('../services/AuthService/userExists.service');
-const { errorResponse, successResponse } = require('../util/response_handler');
 
 /**
- * Sign up a user
- * @param {request} req Express request object
- * @param {response} res Express response object
- * @param {NextFunction} next Next function
- * @returns *
+ * Registers a user
+ * @param req
+ * @param res
+ * @param next
+ * @returns {Promise<void|*>}
  */
-exports.signUp = async (req, res, next) => {
+const signUp = async (req, res, next) => {
   const { email, phone_number } = req.body;
 
-  try {
-    const duplicateUser = await userExists(email, phone_number);
-    if (duplicateUser) {
-      return errorResponse(res, 'User already exists', 409);
-    }
+  const duplicateUser = await userExists(email, phone_number);
 
-    const user = await signUpService(req.body);
-
-    // TODO: send email or sms that a new user just signed up
-
-    return successResponse(res, 201, user);
-  } catch (error) {
-    errorResponse(res, 'Something went wrong', 500);
-    return next(error);
+  if (duplicateUser) {
+    const err = new AppError('User already exists', 409);
+    return next(err);
   }
+
+  const user = await signUpService(req.body);
+
+  // TODO: send email or sms that a new user just signed up
+
+  return successResponse(res, 201, user);
 };
 
 /**
- * Sign in a user
- * @param {request} req Express request object
- * @param {response} res Express response onject
- * @param {NextFunction} next Next function
- * @returns *
+ * Signs in a user
+ * @param req
+ * @param res
+ * @param next
+ * @returns {Promise<void|*>}
  */
-exports.signIn = async (req, res, next) => {
+const login = async (req, res, next) => {
   const { email, phone_number, password } = req.body;
 
-  try {
-    const user = await userExists(email, phone_number);
-    const isMatched = await bcrypt.compare(password, user.password);
+  const user = await userExists(email, phone_number);
+  const isMatched = user && (await bcrypt.compare(password, user.password));
 
-    if (user && isMatched) {
-      const signedInUser = loginService(user);
+  if (user && isMatched) {
+    const signedInUser = await loginService(user);
 
-      // TODO: Send email or sms on user sign in.
+    // TODO: Send email or sms on user sign in.
 
-      return successResponse(res, 201, signedInUser);
-    }
-    return errorResponse(res, 'Invalid credentials', 401);
-  } catch (error) {
-    errorResponse(res, 'Something went wrong', 500);
-    return next(error);
+    return successResponse(res, 201, signedInUser);
   }
+  // If user does not exist
+  const err = new AppError('Invalid Credentials', 401);
+  return next(err);
 };
+
+module.exports = { login: catchAsync(login), signUp: catchAsync(signUp) };
