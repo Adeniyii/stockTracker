727df31c7933dbb80d3887ae2f4805e7ea7854d3
Adeniyii <ifedayoadeniyi@gmail.com>
2021-10-24 19:07:45 +0100

    :sparkles: [feat] implement update user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const AppError = require('../util/appError');
 const catchAsync = require('../util/catchAsync');
 const { findUserById } = require('../services/UserService');
@@ -29,4 +30,53 @@ const getSingleUser = async (req, res, next) => {
   return successResponse(res, 200, { user: requestedUser });
 };
 
-module.exports = { getSingleUser: catchAsync(getSingleUser) };
+/**
+ * Update a single user.
+ * @param req
+ * @param res
+ * @param next
+ * @returns *
+ */
+const updateSingleUser = async (req, res, next) => {
+  const { user_id } = req.params;
+  const requestingUser = await findUserById(req.user.user_id);
+
+  // only super admins can update users with a different id from theirs
+  if (req.user.user_id !== user_id && requestingUser.is_super_admin === false) {
+    return next(new AppError('Permission denied', 403));
+  }
+
+  // Retrieve user to update
+  const requestedUser = await findUserById(user_id);
+
+  if (!requestedUser) {
+    return next(new AppError('User not found', 404));
+  }
+
+  let { phone_number, email, password, first_name, last_name } = req.body;
+
+  if (password) {
+    password = await bcrypt.hash(password, 10);
+  }
+
+  // Updated payload
+  const update = {
+    phone_number,
+    email,
+    first_name,
+    last_name,
+    password,
+  };
+
+  // Update user document
+  // eslint-disable-next-line no-undef
+  const updated = await updateUser(requestedUser, update);
+
+  return successResponse(res, 200, { user: updated });
+};
+
+// Exports
+module.exports = {
+  getSingleUser: catchAsync(getSingleUser),
+  updateSingleUser: catchAsync(updateSingleUser),
+};
