96675cd8e8e91f23795404c326f3b0a8e9173a10
Adeniyii <ifedayoadeniyi@gmail.com>
2021-10-24 17:12:53 +0100

    :lock: [feat] implement route protection middleware

---
+++ auth/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const AppError = require('../util/appError');
+
+const { ACCESS_TOKEN_KEY } = process.env;
+
+const verifyToken = (req, res, next) => {
+  const token =
+    req.body.token ||
+    req.query.token ||
+    req.headers.Authorization.split(' ')[1];
+
+  if (!token) {
+    return next(
+      new AppError('Access token is required for authentication', 403),
+    );
+  }
+  try {
+    const decoded = jwt.verify(token, ACCESS_TOKEN_KEY);
+    req.user = decoded;
+  } catch (err) {
+    return next(new AppError('Invalid token', 401));
+  }
+  return next();
+};
+
+module.exports = verifyToken;
