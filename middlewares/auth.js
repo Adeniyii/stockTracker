c5413769922cc1bb8fcea49e5a43b67037b66ecc
Adeniyii <ifedayoadeniyi@gmail.com>
2021-10-24 12:24:25 +0100

    :sparkles: [feat] implement login request validation
    Validates the request body when a login request is made

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -11,3 +11,14 @@ exports.validateUserSignIn = () => [
   ),
   body('password').isLength({ min: 6 }),
 ];
+
+/**
+ * Validate login request body
+ */
+exports.validateUserLogin = () => [
+  body('password').exists(),
+  oneOf(
+    [body('email').isEmail(), body('phone_number').isMobilePhone()],
+    'phone_number or email required',
+  ),
+];
