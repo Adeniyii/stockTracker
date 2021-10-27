8aedf4777ba7720a2463665939936247d7647d03
Adeniyii <ifedayoadeniyi@gmail.com>
2021-10-27 23:33:54 +0100

    :art: [chore] small architectural updates

--- models/portfolio.model.js
+++ models/portfolio.model.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
 
 const portfolioSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  cash: { type: Number, default: 10000 },
-  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
+  name: {
+    type: String,
+    required: true,
+  },
+  cash: {
+    type: Number,
+    default: 10000,
+  },
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
 });
 
 module.exports = mongoose.model('Portfolio', portfolioSchema);
--- models/trade.model.js
+++ models/trade.model.js
@@ -1,11 +1,26 @@
 const mongoose = require('mongoose');
 
 const tradeSchema = new mongoose.Schema({
-  symbol: { type: String, required: true },
-  purchase_price: { type: Number, required: true },
-  shares: { type: Number, required: true },
-  portfolio_id: { type: mongoose.Schema.Types.ObjectId, ref: 'portfolio' },
-  purchase_date: { type: Date, default: Date.now },
+  symbol: {
+    type: String,
+    required: true,
+  },
+  purchase_price: {
+    type: Number,
+    required: true,
+  },
+  shares: {
+    type: Number,
+    required: true,
+  },
+  portfolio_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'portfolio',
+  },
+  purchase_date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model('Trade', tradeSchema);
