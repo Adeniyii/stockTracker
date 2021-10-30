6ac4672de2539e085b86122d997a0fca5b4854cf
Adeniyii <ifedayoadeniyi@gmail.com>
2021-10-30 15:30:17 +0100

    :memo: [doc] document the getPortfolio routes

--- routes/PortfolioRoute/getPortfolio.js
+++ routes/PortfolioRoute/getPortfolio.js
@@ -10,6 +10,42 @@ const {
   validateGetAllPortfolio,
 } = require('../../middlewares/portfolioValidator');
 
+/**
+ * @swagger
+ * /portfolio/:user_id/all:
+ *   get:
+ *     tags:
+ *      - Portfolio
+ *     summary: Get all user's portfolio
+ *     description: Get all stock portfolios of the logged in user
+ *     parameters:
+ *      - $ref: '#/components/parameters/user_id'
+ *     responses:
+ *      '200':
+ *        description: Portfolios fetched successfully
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Portfolio'
+ *      '403':
+ *        description: The requesting user is unauthorized to update the requested user
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ *      '404':
+ *        description: The user has no portfolios
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ *      default:
+ *        description: An unexpected error occured
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ */
 router.get(
   '/:user_id/all',
   auth,
@@ -17,6 +53,43 @@ router.get(
   bodyValidator,
   getAllPortfolio,
 );
+
+/**
+ * @swagger
+ * /portfolio/:user_id:
+ *   get:
+ *     tags:
+ *      - Portfolio
+ *     summary: Get a single portfolio
+ *     description: Get a single stock portfolio of the logged in user
+ *     parameters:
+ *      - $ref: '#/components/parameters/user_id'
+ *     responses:
+ *      '200':
+ *        description: Portfolios fetched successfully
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Portfolio'
+ *      '403':
+ *        description: The requesting user is unauthorized to update the requested user
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ *      '404':
+ *        description: The user has no portfolios
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ *      default:
+ *        description: An unexpected error occured
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ */
 router.get(
   '/:user_id',
   auth,
