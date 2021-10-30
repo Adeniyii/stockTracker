02bc7c0b60e43fbd577ddef54865cae447655a67
Adeniyii <ifedayoadeniyi@gmail.com>
2021-10-30 16:42:52 +0100

    :memo: [doc] document the getPortfolioPositions route

--- routes/TradeRoute/getPositions.js
+++ routes/TradeRoute/getPositions.js
@@ -8,8 +8,45 @@ const {
   validatePortfolioPositions,
 } = require('../../middlewares/tradeValidator');
 
+/**
+ * @swagger
+ * /trade/:user_id/:
+ *   get:
+ *     tags:
+ *      - Trade
+ *     summary: Get a users portfolio positions
+ *     description: Get all trades for a single portfolio
+ *     parameters:
+ *      - $ref: '#/components/parameters/user_id'
+ *      - $ref: '#/components/parameters/portfolio_id'
+ *     responses:
+ *      '200':
+ *        description: Portfolio positions fetched successfully
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/PortfolioPositions'
+ *      '403':
+ *        description: The requesting user is unauthorized to update the requested user
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ *      '404':
+ *        description: Portfolio not found
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ *      default:
+ *        description: An unexpected error occured
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ */
 router.get(
-  '/:user_id/positions',
+  '/:user_id',
   auth,
   validatePortfolioPositions(),
   bodyValidator,
