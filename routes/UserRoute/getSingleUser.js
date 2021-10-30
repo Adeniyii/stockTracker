92a594350f74d77bcf28f02047895dd42104bed8
Adeniyii <ifedayoadeniyi@gmail.com>
2021-10-30 13:29:18 +0100

    :memo: [docs] document the getSingleUser route

--- routes/UserRoute/getSingleUser.js
+++ routes/UserRoute/getSingleUser.js
@@ -4,6 +4,42 @@ const bodyValidator = require('../../middlewares/validationHandler');
 const { getSingleUser } = require('../../controllers/user.controller');
 const { userIdValidation } = require('../../middlewares/userValidator');
 
+/**
+ * @swagger
+ * /user/:user_id:
+ *   get:
+ *     tags:
+ *      - User
+ *     summary: Gets a single user
+ *     description: Returns a single user, using the user_id param
+ *     parameters:
+ *      - $ref: '#/components/parameters/user_id'
+ *     responses:
+ *      '200':
+ *        description: Login successful
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/User'
+ *      '403':
+ *        description: The requesting user is unauthorized to view the requested user
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ *      '404':
+ *        description: The requested user does not exist
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ *      default:
+ *        description: An Unexpected error occured
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ */
 router.get('/:user_id', auth, userIdValidation(), bodyValidator, getSingleUser);
 
 module.exports = router;
