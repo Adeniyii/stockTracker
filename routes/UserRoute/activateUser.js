8eb4c07de47dc18bb1c41de44cee10f2ebbfd7f0
Adeniyii <ifedayoadeniyi@gmail.com>
2021-10-30 14:16:58 +0100

    :memo: [doc] document the activateUser route

--- routes/UserRoute/activateUser.js
+++ routes/UserRoute/activateUser.js
@@ -4,6 +4,51 @@ const { activateUser } = require('../../controllers/user.controller');
 const { userActivationValidation } = require('../../middlewares/userValidator');
 const bodyValidator = require('../../middlewares/validationHandler');
 
+/**
+ * @swagger
+ * /user/:user_id/activate:
+ *   put:
+ *     tags:
+ *      - User
+ *     summary: Activate a user
+ *     description: Sends an OTP to the users email, to be used for activation
+ *     parameters:
+ *      - $ref: '#/components/parameters/user_id'
+ *     requestBody:
+ *       description: Update payload
+ *       required: false
+ *       content:
+ *         application/json:
+ *           schema:
+ *            $ref: '#/components/schemas/OTP'
+ *     responses:
+ *      '204':
+ *        description: Update successful
+ *      '403':
+ *        description: The requesting user is unauthorized to update the requested user
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ *      '404':
+ *        description: The requested user does not exist
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ *      '405':
+ *        description: User has not enabled 2FA protection
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ *      default:
+ *        description: An Unexpected error occured
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ */
 router.put(
   '/:user_id/activate',
   auth,
