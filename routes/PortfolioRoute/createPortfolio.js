082c839680d98b0006d24957baa7022a645b98e0
Adeniyii <ifedayoadeniyi@gmail.com>
2021-10-30 15:08:18 +0100

    :memo: [doc] document the createPortfolio route

--- routes/PortfolioRoute/createPortfolio.js
+++ routes/PortfolioRoute/createPortfolio.js
@@ -6,8 +6,51 @@ const {
   validateCreatePortfolio,
 } = require('../../middlewares/portfolioValidator');
 
+/**
+ * @swagger
+ * /portfolio/:user_id:
+ *   post:
+ *     tags:
+ *      - Portfolio
+ *     summary: Create a portfolio
+ *     description: Create a new portfolio for the logged in user
+ *     parameters:
+ *      - $ref: '#/components/parameters/user_id'
+ *     requestBody:
+ *       description: Portfolio payload
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *            $ref: '#/components/schemas/Portfolio'
+ *     responses:
+ *      '200':
+ *        description: Portfolio created successfully
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Portfolio'
+ *      '403':
+ *        description: The requesting user is unauthorized to update the requested user
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ *      '409':
+ *        description: The portfolio already exists for user
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ *      default:
+ *        description: An unexpected error occured
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Error'
+ */
 router.post(
-  '/create/:user_id',
+  '/:user_id',
   auth,
   validateCreatePortfolio(),
   bodyValidator,
